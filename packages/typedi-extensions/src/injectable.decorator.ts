bb8ec317d6b490bb11031a84c1660ea9ad0e20fe
Edmar Miyake <edmar.miyake@gmail.com>
2019-01-11 18:20:10 -0200

    typedi warning fixed

--- packages/typedi-extensions/src/injectable.decorator.ts
+++ packages/typedi-extensions/src/injectable.decorator.ts
@@ -8,12 +8,6 @@ export function Injectable() {
   return <TargetType extends ConstructorType>(targetClass: TargetType) => {
     return class extends targetClass {
       constructor(...args: any[]) {
-        console.assert(
-          isReactClassComponent(targetClass),
-          `You're using 'Injectable' on a class that is not a React component. This will probably crash on Safari.
-          targetClass: ${targetClass}`
-        )
-
         const types: any[] = (Reflect as any).getMetadata('design:paramtypes', targetClass) || [];
 
         if (types.length > 0) {
@@ -26,9 +20,4 @@ export function Injectable() {
       }
     };
   };
-}
-
-/** https://stackoverflow.com/a/41658173/3670829 */
-function isReactClassComponent(component) {
-  return typeof component === 'function' && component.prototype.isReactComponent;
 }
